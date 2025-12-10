154fab6f2b4f1dac900bfa05ad1b915db86ef57c
sanoj s <sanojs@MacBookAir.domain.name>
2025-12-10 12:18:26 +0530

    Fix sitemap & robots routing order

--- server/static.ts
+++ server/static.ts
@@ -4,25 +4,30 @@ import path from "path";
 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
+
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
     );
   }
 
+  // Serve sitemap FIRST
+  app.get("/sitemap.xml", (_, res) => {
+    res.setHeader("Content-Type", "application/xml");
+    res.sendFile("sitemap.xml", { root: distPath });
+  });
+
+  // Serve robots FIRST
+  app.get("/robots.txt", (_, res) => {
+    res.setHeader("Content-Type", "text/plain");
+    res.sendFile("robots.txt", { root: distPath });
+  });
+
+  // Serve static files
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
+  // Fallback to index.html
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
-  app.get("/sitemap.xml", (_, res) => {
-  res.setHeader("Content-Type", "application/xml");
-  res.sendFile("sitemap.xml", { root: "./dist/public" });
-});
-app.get("/robots.txt", (_, res) => {
-  res.setHeader("Content-Type", "text/plain");
-  res.sendFile("robots.txt", { root: "./dist/public" });
-});
-
 }
